bb8f0ed90a0d8c3d2f780a3d28ae68da854fb463
jake-duchesne <jake14.d@gmail.com>
2020-08-26 16:05:14 -0700

    implemented petergate for roles, added login helper

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,9 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  access  all:            [:show, :index], 
+          user: {except:  [:destroy, :new, :create, :update, :edit]},
+          editor:         [:create, :delete, :edit],
+          site_admin:     [:all]
 
   # GET /articles
   # GET /articles.json
