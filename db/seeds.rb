0192ab4ec37375eba3562661c25e89bf9c1afc89
jake-duchesne <jake14.d@gmail.com>
2020-08-26 14:49:08 -0700

    small fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 3.times do |f|
   Article.create!(
-    title: "Article #{f}"
-    body: "Some content for #{f}"
-    category: "Category #{f}"
-    username: "Submitted by: #{f}"
+    title: "Article #{f}",
+    body: "Some content for #{f}",
+    category: "Category #{f}",
+    username: "Submitted by: #{f}",
     user_id: "id_#{f}"
   )
 end
