02e3654411dc29578e3738d8f1a9ea9f1dc21a24
jake-duchesne <jake14.d@gmail.com>
2020-08-26 14:36:25 -0700

    added article scaffold system

---
+++ db/migrate/20200826212353_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.string :category
+      t.string :username
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
