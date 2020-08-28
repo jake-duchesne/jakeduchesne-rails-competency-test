86a80b899096c507cc67fea1a0b4c22e7b66e093
jake-duchesne <jake14.d@gmail.com>
2020-08-28 15:18:23 -0700

    updated tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -17,7 +17,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
 
   test "should create article" do
     assert_difference('Article.count') do
-      post articles_url, params: { article: { body: @article.body, category: @article.category, title: @article.title, user_id: @article.user_id, username: @article.username } }
+      post articles_url, params: { article: { body: @article.body, category: @article.category, title: @article.title, user_id: @article.user_id } }
     end
 
     assert_redirected_to article_url(Article.last)
@@ -34,7 +34,7 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update article" do
-    patch article_url(@article), params: { article: { body: @article.body, category: @article.category, title: @article.title, user_id: @article.user_id, username: @article.username } }
+    patch article_url(@article), params: { article: { body: @article.body, category: @article.category, title: @article.title, user_id: @article.user_id } }
     assert_redirected_to article_url(@article)
   end
 
